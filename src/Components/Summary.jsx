e5857e9553e65845a0e130bb853a2b7803e0cba1
Victoria Lo <victorialo398@hotmail.com>
2018-11-14 23:27:07 -0500

    made style changes

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -8,30 +8,40 @@ class Summary extends Component {
         <h1 id="why-me"> Why Me?</h1>
         <div className="flex-container-summ">
           <div className="summary-point">
+            <div className="summary-icon">
+              <i className="fas fa-book-reader" />
+            </div>
             <p>Avid self learner with a passion to succeed</p>
           </div>
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>Passion and eagerness to learn new technical skills</p>
           </div>
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>Excellent leadership, communication, and teamwork skills</p>
           </div>
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>2+ years of work experience</p>
           </div>
         </div>
         <div className="flex-container-summ">
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>7+ years of community involvement</p>
           </div>
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>Strong attention to detail</p>
           </div>
           <div className="summary-point">
+            <div className="summary-icon" />
             <p>Customer service mindset</p>
           </div>
           <div className="summary-point">
-            <p>High aptitidue for creative, outside-the-box thinking</p>
+            <div className="summary-icon" />
+            <p>High aptitude for creative, outside-the-box thinking</p>
           </div>
         </div>
       </div>
