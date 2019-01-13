cbc1469049aefe092a56e9047a16f800fd881045
Victoria Lo <victorialo398@hotmail.com>
2019-01-12 21:26:21 -0500

    add react bootstrap

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  Col,
+  FormControl,
+  ControlLabel,
+  Button
+} from "react-bootstrap";
+import "./Contact.css";
 
 class Contact extends Component {
   render() {
     return (
-      <div id="contact">
-        <div className="contact-form">
-          <h1>Contact Me!</h1>
-        </div>
+      <div className="contact">
+        <h1> Contact Me! </h1>
+        <Form horizontal id="contact-form">
+          <FormGroup controlId="fname">
+            <Col componentClass={ControlLabel} sm={2}>
+              First Name
+            </Col>
+            <Col sm={2}>
+              <FormControl type="text" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="lname">
+            <Col componentClass={ControlLabel} sm={2}>
+              Last Name
+            </Col>
+            <Col sm={2}>
+              <FormControl type="text" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="message">
+            <Col componentClass={ControlLabel} sm={2}>
+              Your Message
+            </Col>
+            <Col sm={5}>
+              <FormControl componentClass="textarea" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">Submit</Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
