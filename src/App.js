c5c347ee4954db9ce4409cd272a79c51f91e68fc
Victoria Lo <victorialo398@hotmail.com>
2019-03-08 23:04:14 -0500

    removed unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-//import { Router, Route } from "react-router-dom";
 import Home from "./Components/Home.jsx";
 import About from "./Components/About.jsx";
-// import Contact from "./Components/Contact";
 import Passions from "./Components/Passions.jsx";
 import Work from "./Components/Work.jsx";
 import Summary from "./Components/Summary.jsx";
 import Ecs from "./Components/Ecs.jsx";
 import Footer from "./Components/Footer.jsx";
-
-//import { Grid, Row, Col } from "react-flexbox-grid";
-
-// import Navigation from "./Components/Navigation";
-//import { HashLink as Link } from "react-router-hash-link";
 
 class App extends Component {
   render() {
@@ -25,18 +18,8 @@ class App extends Component {
         <Work />
         <Ecs />
         <Passions />
-        {/* <Contact /> */}
         <Footer />
       </div>
-      //<Router>
-
-      // <div className="routes">
-      //   <Route path="/contact" component={Contact} />
-      //   <Route path="/resume" component={Resume} />
-      //   <Route path="/about" component={About} />
-      //   <Route path="/" component={Home} />
-      // </div>
-      // </Router>
     );
   }
 }
