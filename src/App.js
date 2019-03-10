bdb993cfb7910173b12691a5a8799765597a05f9
Victoria Lo <victorialo398@hotmail.com>
2019-03-10 01:19:39 -0500

    reconfigured work component

--- src/App.js
+++ src/App.js
@@ -7,15 +7,32 @@ import Work from "./Components/Work.jsx";
 import Summary from "./Components/Summary.jsx";
 import Ecs from "./Components/Ecs.jsx";
 import Footer from "./Components/Footer.jsx";
+import WorkData from "./Components/WorkData.js";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      WorkItems: WorkData
+    };
+  }
   render() {
+    const WIlist = this.state.WorkItems.map(item => (
+      <Work key={item.id} item={item} />
+    ));
     return (
       <div id="scroll-sections">
         <Home />
         <About />
         <Summary />
-        <Work />
+        <div className="work-component">
+          <h1 className="work-header">Work Experience</h1>
+          <p id="work-subheader">
+            {" "}
+            Hover to learn more about some of the things I did on the job!
+          </p>
+          {<div id="small-work-components">{WIlist}</div>}
+        </div>
         <Ecs />
         <Passions />
         <Footer />
