c2f3813d4decb47fc58d41af20ef21357db6471d
Victoria Lo <victorialo398@hotmail.com>
2019-03-10 01:21:14 -0500

    restructured work data

---
+++ src/Components/WorkData.js
@@ -0,0 +1,35 @@
+const WorkData = [
+  {
+    id: 1,
+    company: "Polar",
+    title: "Software Developer",
+    descOne:
+      "Design, implement, and spearhead the automation process of Polar’s site preview product using Python (Django), reducing workflow process duration from 2 hours to 10 minutes",
+    descTwo:
+      "Build high quality, scalable, and responsive native advertisments and demo sites for publisher clients using HTML/CSS, Javascript, JQuery, and  Polar API",
+    descThree:
+      "Meet with Product, Engineering, and Sales department teams to optimize product implementations "
+  },
+  {
+    id: 2,
+    company: "BMO",
+    title: "Reporting Data Analyst",
+    descOne:
+      "Perform aggregation of KPIs across assigned technology portfolios, and present key performance metrics, issues, and risks to CIO and lead technology officers",
+    descTwo:
+      "Improve reporting accuracy, efficiency, and data quality, using advanced Excel features (VBA, macros, pivot tables, vlookup), <b>reducing QA time by 90%",
+    descThree: "Other tools: VBA, Excel (Macros), SharePoint, TIBCO Spotfire"
+  },
+  {
+    id: 3,
+    company: "Aviva",
+    title: "Business Analyst",
+    descOne:
+      " Interview underwriters, business managers, and project managers to gather, assimilate and document pertinent information for the new Commercial Lines (CL) system (RealTime Gateway 2)",
+    descTwo:
+      " Followed agile methodology on all projects and used JIRA for tracking and project managament - consistently met deadlines 1 week earlier",
+    descThree:
+      " Compared and integrated rating basis data from Lotus and SharePoint platforms"
+  }
+];
+export default WorkData;
