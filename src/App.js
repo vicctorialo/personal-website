dcac2536bdf36c0cbc40a9eac9ee6f6f861271e6
Victoria Lo <victorialo398@hotmail.com>
2019-03-11 01:30:10 -0400

    restructured summary component

--- src/App.js
+++ src/App.js
@@ -8,23 +8,31 @@ import Summary from "./Components/Summary.jsx";
 import Ecs from "./Components/Ecs.jsx";
 import Footer from "./Components/Footer.jsx";
 import WorkData from "./Components/WorkData.js";
+import SummaryData from "./Components/SummaryData.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      WorkItems: WorkData
+      WorkItems: WorkData,
+      SummaryItems: SummaryData
     };
   }
   render() {
     const WIlist = this.state.WorkItems.map(item => (
       <Work key={item.id} item={item} />
     ));
+    const SList = this.state.SummaryItems.map(item => (
+      <Summary key={item.id} item={item} />
+    ));
     return (
       <div id="scroll-sections">
         <Home />
         <About />
-        <Summary />
+        <div className="summary-component">
+          <h1 id="why-me"> Why Me?</h1>
+          <div id="small-summary-components">{SList}</div>
+        </div>
         <div className="work-component">
           <h1 className="work-header">Work Experience</h1>
           <p id="work-subheader">
