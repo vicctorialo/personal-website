9f1c1598ea189d13c50ae11d60c112b6a1114069
Victoria Lo <victorialo398@hotmail.com>
2019-03-11 01:29:17 -0400

    added summary data

---
+++ src/Components/SummaryData.js
@@ -0,0 +1,43 @@
+const SummaryData = [
+  {
+    id: 1,
+    icon: "fas fa-book-reader fa-4x",
+    point: "Avid self learner with a passion to succeed"
+  },
+  {
+    id: 2,
+    icon: "fas fa-laptop-code fa-4x",
+    point: "Passion and eagerness to learn new technical skills"
+  },
+  {
+    id: 3,
+    icon: "fas fa-comments fa-4x",
+    point: "Excellent leadership, communication, and teamwork skills"
+  },
+  {
+    id: 4,
+    icon: "fas fa-briefcase fa-4x",
+    point: "2+ years of work experience"
+  },
+  {
+    id: 5,
+    icon: "fas fa-users fa-4x",
+    point: "7+ years of community involvement"
+  },
+  {
+    id: 6,
+    icon: "fas fa-glasses fa-4x",
+    point: "Strong attention to detail"
+  },
+  {
+    id: 7,
+    icon: "fas fa-star fa-4x",
+    point: "Customer service mindset"
+  },
+  {
+    id: 8,
+    icon: "fas fa-lightbulb fa-4x",
+    point: "High aptitude for creative, outside-the-box thinking"
+  }
+];
+export default SummaryData;
