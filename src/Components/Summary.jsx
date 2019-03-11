7b0b97338f74f981721a89bfdbf55b83e2e6b08d
Victoria Lo <victorialo398@hotmail.com>
2019-03-11 01:29:38 -0400

    restructured component

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -5,58 +5,11 @@ class Summary extends Component {
   render() {
     return (
       <div className="summary">
-        <h1 id="why-me"> Why Me?</h1>
-        <div className="flex-container-summ">
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i className="fas fa-book-reader fa-4x" />
-            </div>
-            <p>Avid self learner with a passion to succeed</p>
+        <div className="summary-point">
+          <div className="summary-icon">
+            <i className={this.props.item.icon} />
           </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i className="fas fa-laptop-code fa-4x" />
-            </div>
-            <p>Passion and eagerness to learn new technical skills</p>
-          </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i class="fas fa-comments fa-4x" />
-            </div>
-            <p>Excellent leadership, communication, and teamwork skills</p>
-          </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i className="fas fa-briefcase fa-4x" />
-            </div>
-            <p>2+ years of work experience</p>
-          </div>
-        </div>
-        <div className="flex-container-summ">
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i className="fas fa-users fa-4x" />
-            </div>
-            <p>7+ years of community involvement</p>
-          </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i class="fas fa-glasses fa-4x" />
-            </div>
-            <p>Strong attention to detail</p>
-          </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i class="fas fa-star fa-4x" />
-            </div>
-            <p>Customer service mindset</p>
-          </div>
-          <div className="summary-point">
-            <div className="summary-icon">
-              <i className="fas fa-lightbulb fa-4x" />
-            </div>
-            <p>High aptitude for creative, outside-the-box thinking</p>
-          </div>
+          <p>{this.props.item.point}</p>
         </div>
       </div>
     );
